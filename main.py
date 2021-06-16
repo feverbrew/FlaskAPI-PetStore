0a975e44117edc384d2387c31520e6d3c72bcd81
feverbrew <33206171+feverbrew@users.noreply.github.com>
2021-06-16 11:22:40 -0400

    Store implemented
    Finished the last API section. Testing required.

--- main.py
+++ main.py
@@ -10,6 +10,11 @@ class Status(str, Enum):
     PENDING = "PENDING"
     SOLD = "SOLD"
 
+class Order_Status(str, Enum):
+    PLACED = "PLACED"
+    APPROVED = "APPROVED"
+    DELIVERED = "DELIVERED"
+
 pets_args = reqparse.RequestParser()
 pets_args.add_argument("id",type=int)
 pets_args.add_argument("name", type=str, required=True)
@@ -41,8 +46,17 @@ status_arg.add_argument("statuses", type=Status, action = 'append')
 userlist_args = reqparse.RequestParser()
 userlist_args.add_argument("userlist", type=list)
 
+order_args = reqparse.RequestParser()
+order_args.add_argument("id", type=int)
+order_args.add_argument("petId", type=int)
+order_args.add_argument("quantity", type=int)
+order_args.add_argument("shipDate", type=str)
+order_args.add_argument("status", type=Order_Status)
+order_args.add_argument("complete", type=bool)
+
 pets = {}
 users = {}
+orders = {}
 logged_in_users = []
 
 def pet_id_dne(id):
@@ -64,6 +78,14 @@ def username_exists(username):
 def loggedIn(username):
     users[username].status = 1
     logged_in_users.append(username)
+
+def order_dne(order_id):
+    if order_id not in orders:
+        abort(400, "Order does not exist!")
+
+def order_exists(order_id):
+    if order_id in orders:
+        abort(405, "Order ID already exists!")
 
 class Pet(Resource):
     def put(self):
@@ -131,10 +153,16 @@ class UserActions(Resource):
         username_dne(username)
         if users[username] in logged_in_users:
             users[username] = user_args.parse_args()
+            return users[username]
+        else:
+            return "User not logged in", 400
     def delete(self, username):
         username_dne(username)
         if users[username] in logged_in_users:
             users.pop(username)
+            return '', 204
+        else:
+            return "User not logged in", 400
 
 class UserLogin(Resource):
     def get(self):
@@ -151,18 +179,47 @@ class UserLogout(Resource):
             users[user].status = 0
         logged_in_users.clear()
 
+class StorePlaceOrder(Resource):
+    def post(self):
+        order = order_args.parse_args()
+        order_exists(order.id)
+        orders[order.id] = order
+        return order[order.id]
+
+class StoreOrderActions(Resource):
+    def get(self, id):
+        order_dne(id)
+        return orders[id]
+    def delete(self, id):
+        order_dne(id)
+        orders.pop(id)
+        return '', 204
+
+class StoreInventory(Resource):
+    def get(self):
+        inventory = {"AVAILABLE":0, "PENDING":0, "SOLD":0}
+        for id in pets:
+            if pets[id].status != None:
+                inventory[pets[id].status] += 1
+        return jsonify(inventory)
+
+
 
 
 api.add_resource(Pet, "/pet")
 api.add_resource(PetID, "/pet/<int:id>")
 api.add_resource(PetIDImage, "/pet/<int:id>/uploadImage")
 api.add_resource(StatusFilter, "/pet/findByStatus")
+
 api.add_resource(User, "/user")
 api.add_resource(UserList, "/user/createWithList")
 api.add_resource(UserActions, "/user/<string:username>")
 api.add_resource(UserLogin, "/user/login")
 api.add_resource(UserLogout, "/user/logout")
 
+api.add_resource(StorePlaceOrder, "/store/order")
+api.add_resource(StoreOrderActions, "/store/order/<int:id>")
+api.add_resource(StoreInventory, "/store/inventory")
 
 if __name__ == "__main__":
     app.run(debug=True)
